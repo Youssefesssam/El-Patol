b00b5ede440ac8f31dfbd0bc8b7bab254a44eb13
Youssef Emad <yemad1647@gmail.com>
2025-08-18 07:14:41 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -54,11 +54,6 @@ import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-
-
-
-
-
 import 'l10n/app_localizations.dart';
 import 'l10n/l10n.dart';
 
